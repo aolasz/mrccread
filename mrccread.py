29fe7fc88cbb3f96086041919713e823aa6b79aa
aolasz <49680062+aolasz@users.noreply.github.com>
2022-04-20 19:50:18 +0200

    Format with

--- mrccread.py
+++ mrccread.py
@@ -10,7 +10,7 @@ import xlsxwriter
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "outfiles",
-    nargs='+',
+    nargs="+",
     type=argparse.FileType("rt"),
     default=sys.stdin,
     help="Path to one or more MRCC output files",
@@ -25,28 +25,26 @@ parser.add_argument(
 args = parser.parse_args()
 fpaths = [Path(outfile.name).resolve() for outfile in args.outfiles]
 
-header0 = ["Molecule", "Method", "Basis", "CPU time", "Memory",	"Disk load", "Energy",	"Determinants"]
+header0 = [
+    "Molecule",
+    "Method",
+    "Basis",
+    "CPU time",
+    "Memory",
+    "Disk load",
+    "Energy",
+    "Determinants",
+]
 header1 = [None, None, None, "[s]", "[MiB]", "[MiB]", "[A.U.]", None]
 headers = (header0, header1)
 
 
 def keyword(kw):
-    return re.compile(
-        fr"^\s?{kw}\s?=\s?([^#\n\r\s]+)",
-        flags=
-            re.IGNORECASE |
-            re.MULTILINE
-    )
+    return re.compile(rf"^\s?{kw}\s?=\s?([^#\n\r\s]+)", flags=re.IGNORECASE | re.MULTILINE)
 
 
 def find(*, kw, text):
-    search = re.search(
-        fr"^\s?{kw}\s?=\s?([^#\n\r\s]+)",
-        text,
-        flags=
-            re.IGNORECASE |
-            re.MULTILINE
-    )
+    search = re.search(rf"^\s?{kw}\s?=\s?([^#\n\r\s]+)", text, flags=re.IGNORECASE | re.MULTILINE)
     if not search:
         raise KeyError(kw)
     return search.group(1)
@@ -54,11 +52,9 @@ def find(*, kw, text):
 
 def get_energy(*, text):
     search = re.findall(
-    r"^\s?Total\s[\w\d)(\]\[]+\senergy\s\[au\]:\s+(-?\d+\.\d*)",
-    text,
-    flags=
-        re.IGNORECASE |
-        re.MULTILINE
+        r"^\s?Total\s[\w\d)(\]\[]+\senergy\s\[au\]:\s+(-?\d+\.\d*)",
+        text,
+        flags=re.IGNORECASE | re.MULTILINE,
     )
     if len(search) < 1:
         raise KeyError(method)
@@ -67,11 +63,7 @@ def get_energy(*, text):
 
 def get_num_determinants(*, text):
     search = re.findall(
-        r"^\s?Total number of determinants:\s+(\d+)",
-        text,
-        flags=
-            re.IGNORECASE |
-            re.MULTILINE
+        r"^\s?Total number of determinants:\s+(\d+)", text, flags=re.IGNORECASE | re.MULTILINE
     )
     if len(search) < 1:
         raise KeyError(method)
@@ -81,8 +73,8 @@ def get_num_determinants(*, text):
 workbook = xlsxwriter.Workbook(args.xlsxfile)
 worksheet = workbook.add_worksheet()
 
-bold = workbook.add_format({'bold': True})
-worksheet.set_column('B:B', 20)
+bold = workbook.add_format({"bold": True})
+worksheet.set_column("B:B", 20)
 
 for header_num, header in enumerate(headers):
     worksheet.write_row(header_num, 0, header, bold)
@@ -98,7 +90,7 @@ for fnum, fpath in enumerate(fpaths):
     basis = find(kw="basis", text=out_text).upper()
     num_determinants = get_num_determinants(text=out_text)
     energy = get_energy(text=out_text)
-    
+
     worksheet.write(row_num, 0, molecule)
     worksheet.write(row_num, 1, calc)
     worksheet.write(row_num, 2, basis)
