f5b8eedf98e55dc2fdd146c69079527bc089d3c1
aolasz <49680062+aolasz@users.noreply.github.com>
2022-04-20 21:09:46 +0200

    Fix: add reST docstrings

--- mrccread.py
+++ mrccread.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+"""
+MRCC out file parser
+This script allows the user to parse some information of MRCC out files and collect the results
+in an xlsx file.
+This script accepts a list of out files (.out).
+The results are written in an xlsx file or to the stdout if no xlsx file is given.
+Pre-requisite :
+xlsxwriter package to be installed within the Python runtime environment
+"""
+
 import argparse
 from pathlib import Path
 import re
@@ -39,16 +49,29 @@ header1 = [None, None, None, "[s]", "[MiB]", "[MiB]", "[A.U.]", None]
 headers = (header0, header1)
 
 
-def find(*, kw, text):
-    search = re.search(
-        rf"^\s?{kw}\s?=\s?([^#\n\r\s]+)", text, flags=re.IGNORECASE | re.MULTILINE
-    )
+def find(*, kw: str, text: str) -> str:
+    """
+    Find the value of a keyword in an MRCC output text
+
+    :param kw: the keyword whose value we are looking for
+    :param text: text of the MRCC out file
+    :returns: the text value of keyword found first
+    :raises KeyError: if the keyword is not found
+    """
+    search = re.search(rf"^\s?{kw}\s?=\s?([^#\n\r\s]+)", text, flags=re.IGNORECASE | re.MULTILINE)
     if not search:
         raise KeyError(kw)
     return search.group(1)
 
 
-def get_energy(*, text):
+def get_energy(*, text: str) -> float:
+    """
+    Find the last "Total energy" value in an MRCC output text
+
+    :param text: text of the MRCC out file
+    :returns: the float value of the last "Total energy"
+    :raises KeyError: if no "Total energy" is found
+    """
     search = re.findall(
         r"^\s?Total\s[\w\d)(\]\[]+\senergy\s\[au\]:\s+(-?\d+\.\d*)",
         text,
@@ -59,7 +82,14 @@ def get_energy(*, text):
     return float(search[-1])
 
 
-def get_num_determinants(*, text):
+def get_num_determinants(*, text: str) -> int:
+    """
+    Find the last "Total number of determinants" in an MRCC output text
+
+    :param text: text of the MRCC out file
+    :returns: the integer value of the last "Total number of determinants"
+    :raises KeyError: if no "Total number of determinants" is found
+    """
     search = re.findall(
         r"^\s?Total number of determinants:\s+(\d+)",
         text,
